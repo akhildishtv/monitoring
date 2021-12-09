4c7802e2116cf9da1a079af82f3728f1a72c3c47
Akhil Kumar Gupta <awadhesh.soni@dishtv.in>
2021-12-09 15:00:55 +0530

    new code update

--- src/app/analytics/languages/languages.component.ts
+++ src/app/analytics/languages/languages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { CommonService } from 'src/app/services/common.service';
+import { Color, defaultColors } from 'ng2-charts';
 
 @Component({
   selector: 'app-languages',
@@ -11,16 +12,157 @@ export class LanguagesComponent implements OnInit {
   id: any
   startTime: string;
   diff: number;
+  public mainChartData: Array<any> = [];
+  public mainChartLabels: Array<any> = [];
+  public mainChartLegend = true;
+  public mainChartType = 'line';
+  public mainChartColours: Array<any> = [
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#D0B1AA',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#CFD166',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#768167',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#BFE622',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#0F8434',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#59DFCB',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#4079DD',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#03493E',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#071CEF',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff'
+    },
+    {
+      backgroundColor: 'transparent',
+      borderColor: '#F214E5',
+      pointRadius: 3,
+      pointBorderWidth: 3,
+      pointHoverBackgroundColor: '#fff',
+    },
+  ];
+  public mainChartOptions: any = {
+    tooltips: {
+      enabled: true,
+      itemSort: (a, b, data) => b.yLabel - a.yLabel,
+      intersect: true,
+      mode: 'x',
+      position: 'nearest',
+      borderWidth: 1,
+      caretPadding: 15,
+      colorBody: '#666',
+      displayColors: true,
+      titleFontColor: '#999',
+      titleMarginBottom: 10,
+      xPadding: 15,
+      yPadding: 15,
+      callbacks: {
+        labelColor: function (tooltipItem, chart) {
+          return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor };
+        }
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            stepSize: 0.5,
+            beginAtZero: true
+          },
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: "RESPONSE TIME",
+          },
+        },
+      ],
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "TIME",
+          },
+        },
+      ],
+    },
+    elements: {
+      line: {
+        borderWidth: 2,
+      },
+      point: {
+        radius: 0,
+        hitRadius: 10,
+        hoverRadius: 4,
+        hoverBorderWidth: 3,
+      }
+    },
+    legend: {
+      display: false,
+      maxWidth: 5,
+      labels: {
+        boxWidth: 15,
 
+      }
+    }
+  };
   constructor(
     private APIService: CommonService
   ) { }
 
   ngOnInit(): void {
-    this.getData()
+    this.getData(1)
     this.id = setInterval(() => {
-      this.getData()
-    }, 30000)
+      this.getData(2)
+    }, 10000)
   }
   ngOnDestroy() {
     if (this.id) {
@@ -28,7 +170,7 @@ export class LanguagesComponent implements OnInit {
     }
   }
 
-  getData() {
+  getData(tag) {
     // this.spinner.show();
     let now = new Date();
     var dateStringWithTime = moment(now).format(`yyyy-MM-DDTHH:mm:ss.SSSZ`);
@@ -45,11 +187,30 @@ export class LanguagesComponent implements OnInit {
       hour12: true
     }).format(startTime)
     this.startTime = time1
+    this.mainChartLabels.push(time1)
+    if (this.mainChartLabels.length > 10) {
+      this.mainChartLabels.shift();
+    }
+    this.mainChartColours.push(defaultColors)
     this.APIService.getLanguageList(value)
       .subscribe(data => {
         if (data) {
           const endTime = new Date().getTime();
           this.diff = (endTime - startTime) / 1000
+          let val = []
+          val.push(this.diff)
+          let value = {
+            data: val
+          }
+          if (tag == 1) {
+            this.mainChartData.push(value)
+          }
+          else {
+            this.mainChartData[1].data.push(this.diff)
+            if (this.mainChartData[1].data.length > 10) {
+              this.mainChartData[1].data.shift();
+            }
+          }
           // this.spinner.hide();
         }
       })
